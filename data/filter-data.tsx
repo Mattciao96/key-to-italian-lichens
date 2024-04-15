d555cd0dbe5073348084d48a57a1e4eba38c51a8
Matteo Conti <matt.ciao@gmail.com>
2024-04-15 13:53:44 +0200

    new images

--- data/filter-data.tsx
+++ data/filter-data.tsx
@@ -1,22 +1,46 @@
 export const filterData = [
   {
-    title: 'Group 1',
-    description: 'This is group 1',
+    id: 4,
+    title: "Substratum",
+    description: "This is group 1",
     items: [
-      { text: 'Bark and wood', value: 'bark', image: 'bark.png' },
-      { text: 'Rock', value: 'rock', image: 'rock.png' },
-      { text: 'Soil, terricolous mosses and plant debris', value: 'soil', image: 'soil.png' },
+      { text: "Bark and wood", value: 1, image: "bark.png" },
+      { text: "Rock", value: 3, image: "rock.png" },
+      {
+        text: "Soil, terricolous mosses and plant debris",
+        value: 2,
+        image: "soil.png",
+      },
     ],
   },
   {
-    title: 'Group 2',
-    description: 'This is group 2',
+    id: 45,
+    title: "Thallus",
+    description: "Th",
     items: [
-      { text: 'Option 1', value: 'option1', image: 'bark.png' },
-      { text: 'Option 2', value: 'option2', image: 'rock.png' },
-      { text: 'Option 3', value: 'option3', image: 'soil.png' },
+      { text: "Crustose or leprose", value: 2, image: "crustose.png" },
+      {
+        text: "Squamulose, or (for cyanobacterial lichens only) microfruticose",
+        value: 1,
+        image: "squamulose.png",
+      },
     ],
   },
+  {
+    id: 53,
+    title: "Thallus",
+    description: "Th",
+    depend: {id: 26, item:1},
+    items: [
+      { text: "Crustose-placodioid, with marginal lobes", value: 1, image: "placoid.png" },
+      {
+        text: "Crustose not placodioid",
+        value: 2,
+        image: "not-placoid.png",
+      },
+    ],
+  },
+
   /* {
     title: 'topo',
     description: 'puzzone',
