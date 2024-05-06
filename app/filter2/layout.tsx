88e0f05d27acaf8ce12cd959118f09a56d1b8d45
Matteo Conti <matt.ciao@gmail.com>
2024-05-06 07:42:10 +0200

    chore: Add yet-another-react-lightbox package to dependencies

--- app/filter2/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { Providers } from "@/app/filter2/providers";
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <Providers>{children}</Providers>
-    </>
-  );
-}
--- app/taxa/page.tsx
+++ app/taxa/page.tsx
@@ -1,25 +1,23 @@
 /* eslint-disable @next/next/no-img-element */
 "use client";
+import * as React from 'react'
 import {
   QueryClient,
   QueryClientProvider,
   useQuery,
 } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
-export default function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <ImageList />
-    </QueryClientProvider>
-  );
-}
 
-export function ImageList() {
+export default function ImageList() {
+  const [open, setOpen] = React.useState(false);
+
 
   const taxaFilter = useQuery({
     queryKey: ["taxaFilter"],
+    staleTime: Infinity,
     queryFn: () =>
       fetch("https://italic.units.it/api/v1/taxa-filter", {
         method: "POST",
@@ -36,11 +34,21 @@ export function ImageList() {
   if (taxaFilter.error)
     return "An error has occurred: " + taxaFilter.error.message;
 
+  
+  const lightData = taxaFilter.data.map((taxon) => {
+    return { src: taxon.thumbnail
+      ? `https://italic.units.it/flora/${taxon.thumbnail}`
+      : "placeholder.svg"}
+  })  
+
+  
   return (
     <div className="w-full flex justify-center">
       <ul className="p-2 md:p-4 grid grid-cols-2 md:grid-cols-4 gap-x-2 gap-y-8 max-w-[900px]">
         {taxaFilter.data.map((taxon) => (
-          <li key={taxon.species_id}>
+          <li key={taxon.species_id} onClick={() => {
+            setOpen(true)
+          }}>
             <img
               loading="lazy"
               alt={taxon.name}
@@ -57,6 +65,11 @@ export function ImageList() {
           </li>
         ))}
       </ul>
+      <Lightbox
+        open={open}
+        close={() => setOpen(false)}
+        slides={lightData}
+      />
     </div>
   );
 }
